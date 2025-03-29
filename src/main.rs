c9cfb2e305238e1b704db0a392e938af62dfb270
provokateurin <kate@provokateurin.de>
2025-03-29 08:16:06 +0100

    fix: Deduplicate release groups and display all artists as authors
    Fixes https://github.com/provokateurin/musicbrainz-rss-generator/issues/1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-use musicbrainz_rs::entity::artist::Artist;
 use musicbrainz_rs::entity::release_group::ReleaseGroup;
 use musicbrainz_rs::prelude::*;
 use rss::{ChannelBuilder, GuidBuilder, ItemBuilder};
 use std::cmp::Reverse;
+use std::collections::HashMap;
 use std::io;
-
-struct ArtistReleaseGroup {
-    artist: Artist,
-    release_group: ReleaseGroup,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
@@ -17,7 +12,7 @@ async fn main() -> Result<(), String> {
         .title("MusicBrainz Releases")
         .link("https://musicbrainz.org");
 
-    let mut items: Vec<ArtistReleaseGroup> = Vec::new();
+    let mut keyed_items: HashMap<String, ReleaseGroup> = HashMap::new();
 
     for artist_mbid in io::stdin().lines() {
         let artist_mbid = match artist_mbid {
@@ -25,22 +20,12 @@ async fn main() -> Result<(), String> {
             Err(_) => panic!("Error reading from stdin."),
         };
 
-        let artist = match Artist::fetch().id(artist_mbid.as_str()).execute().await {
-            Ok(artist) => {
-                if artist.id == "" {
-                    panic!("Invalid artist MBID {:?}", artist_mbid);
-                }
-
-                artist
-            }
-            Err(_) => panic!("Error fetching artist with MBID {:?}", artist_mbid),
-        };
-
         let mut offset = 0;
 
         loop {
             let release_groups = match ReleaseGroup::browse()
                 .by_artist(artist_mbid.as_str())
+                .with_artist_credits()
                 // The server will not return more entities than this
                 .limit(100)
                 .offset(offset)
@@ -55,10 +40,7 @@ async fn main() -> Result<(), String> {
             };
 
             for release_group in release_groups.entities.clone() {
-                items.push(ArtistReleaseGroup {
-                    artist: artist.clone(),
-                    release_group,
-                });
+                keyed_items.insert(release_group.clone().id, release_group);
             }
 
             offset += release_groups.entities.len() as u16;
@@ -68,33 +50,39 @@ async fn main() -> Result<(), String> {
         }
     }
 
-    items.sort_by_key(|item| Reverse(item.release_group.first_release_date));
+    let mut items = keyed_items.values().collect::<Vec<&ReleaseGroup>>();
+    items.sort_by_key(|item| Reverse(item.first_release_date));
 
     for item in items {
-        let link = format!(
-            "https://musicbrainz.org/release-group/{}",
-            item.release_group.id
-        );
+        let link = format!("https://musicbrainz.org/release-group/{}", item.id);
+
+        let mut author = "".to_owned();
+        for artist_credit in item.artist_credit.clone().unwrap() {
+            author.push_str(artist_credit.name.as_str());
+            match artist_credit.joinphrase {
+                None => {}
+                Some(join_phrase) => {
+                    author.push_str(join_phrase.as_str());
+                }
+            }
+        }
 
         let mut binding = ItemBuilder::default();
         let item_builder = binding
             .guid(GuidBuilder::default().value(link.clone()).build())
             .link(link)
-            .title(format!(
-                "{}: {}",
-                item.artist.name.clone(),
-                item.release_group.title
-            ))
-            .author(item.artist.name.clone());
+            .title(format!("{}: {}", author, item.title))
+            .author(author);
 
-        if !item.release_group.first_release_date.is_none() {
-            item_builder.pub_date(Some(
-                item.release_group
-                    .first_release_date
-                    .unwrap()
-                    .format("%a, %d %b %Y 00:00:00 UT")
-                    .to_string(),
-            ));
+        match item.first_release_date {
+            None => {}
+            Some(first_release_date) => {
+                item_builder.pub_date(
+                    first_release_date
+                        .format("%a, %d %b %Y 00:00:00 UT")
+                        .to_string(),
+                );
+            }
         }
 
         channel_builder.item(item_builder.build());
